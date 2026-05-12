107d9dcb37973a40b4b5cecbddf5d9d30398c527
marimuthup2024aids-dev <marimuthu.p2024aids@sece.ac.in>
2026-05-12 15:56:07 +0530

    Update App.jsx

--- react-profile-main/src/App.jsx
+++ react-profile-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>AWS</h1>
+          <h1>Marimuthu P</h1>
           <p>
           <code>CI CD</code>
           </p>
